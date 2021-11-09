4e53d82c6d4b6c0e97fbb29bc13bbbcd31951d1e
musai7 <musaishankar307@gmail.com>
2021-11-09 11:34:59 +0530

    added : emp present or abscent logic

---
+++ src/com/bridgeit/emplogics/EmpWageComputation.java
@@ -0,0 +1,13 @@
+package com.bridgeit.emplogics;
+
+public class EmpWageComputation {
+	public static void main(String[] args) {
+		// employee present or absent logic
+		int is_Present = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == is_Present) {
+			System.out.println("employee is present");
+		} else
+			System.out.println("employee is abscent ");
+	}
+}
