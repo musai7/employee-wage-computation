e30146823ff2112d7382254f95fe0909a72867cc
musai7 <musaishankar307@gmail.com>
2021-11-09 11:54:10 +0530

    added : part time emp wage

--- src/com/bridgeit/emplogics/EmpWageComputation.java
+++ src/com/bridgeit/emplogics/EmpWageComputation.java
@@ -1,17 +1,20 @@
 package com.bridgeit.emplogics;
 
 public class EmpWageComputation {
-	public static void main(String[] args) {
-		//Daily Employee Wage Calculation
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		// constants
-		int is_present = 1, empWagePerHr = 20, is_fullTime = 8;
 
+	// added part time employee logic
+	public static void main(String[] args) {
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 		int totalEmpWagePerDay = 0;
+		// constants
+		int is_present = 1, is_partTime = 2, empWagePerHr = 20, is_fullTimeWorkingHrs = 8, is_partTimeWorkingHrs = 4;
 		if (empCheck == is_present) {
-			totalEmpWagePerDay = is_fullTime * empWagePerHr;
-			System.out.println("employee is present and employee wage is : " + totalEmpWagePerDay);
+			totalEmpWagePerDay = is_fullTimeWorkingHrs * empWagePerHr;
+			System.out.println("daily employee wage is : " + totalEmpWagePerDay);
+		} else if (empCheck == is_partTime) {
+			totalEmpWagePerDay = is_partTimeWorkingHrs * empWagePerHr;
+			System.out.println("part time employee wage is : " + totalEmpWagePerDay);
 		} else
-			System.out.println("employee is abscent and employee wage is : " + totalEmpWagePerDay);
+			System.out.println(" employee absent daily employee wage is : " + totalEmpWagePerDay);
 	}
 }
