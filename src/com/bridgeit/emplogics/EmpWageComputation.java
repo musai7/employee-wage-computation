84776c46e68a0bae8afcc5009655d729d8a68923
musai7 <musaishankar307@gmail.com>
2021-11-09 12:12:46 +0530

    added : employee wage

--- src/com/bridgeit/emplogics/EmpWageComputation.java
+++ src/com/bridgeit/emplogics/EmpWageComputation.java
@@ -2,30 +2,36 @@ package com.bridgeit.emplogics;
 
 public class EmpWageComputation {
 
-	public static void main(String[] args) {
-		int totalEmpWagePerDay = 0, totalMontlyWageOfEmp = 0;
-		// constants
-		int empWagePerHr = 20, is_fullTimeWorkingHrs = 8, is_partTimeWorkingHrs = 4, noOfWorkingDays = 20;
+	/* Calculate Wages till a condition of total working hours or days is reached
+	 for a month */
 
-		for (int counter = 1; counter <= noOfWorkingDays; counter++) {
+	public static void main(String[] args) {
+		int totalEmpWagePerDay = 0, totalMontlyWageOfEmp = 0, totalNoOfHrsWorked = 0, totalNoOfDaysWorked = 0;
+
+		int empWagePerHr = 20, is_fullTimeWorkingHrs = 8, is_partTimeWorkingHrs = 4;
+		int totalWorkingHrsInAMonth = 100, workingDaysInAMonth = 20;
+
+		while (totalNoOfDaysWorked < workingDaysInAMonth && totalNoOfHrsWorked < totalWorkingHrsInAMonth) {
 
 			double empCheck = Math.floor(Math.random() * 10) % 3;
 
 			int empSwitchCheck = (int) empCheck;
 
 			switch (empSwitchCheck) {
-				case 1:
-					totalEmpWagePerDay = is_fullTimeWorkingHrs * empWagePerHr;
-					totalMontlyWageOfEmp = totalMontlyWageOfEmp + totalEmpWagePerDay;
-					System.out.println("day" + counter + "employee wage is equal to " + totalEmpWagePerDay);
-					break;
-				case 2:
-					totalEmpWagePerDay = is_partTimeWorkingHrs * empWagePerHr;
-					totalMontlyWageOfEmp = totalMontlyWageOfEmp + totalEmpWagePerDay;
-					System.out.println("day" + counter + "employee wage is equal to " + totalEmpWagePerDay);
-					break;
-				default:
-					System.out.println("day" + counter + "employee absent " + totalEmpWagePerDay);
+			case 1:
+				totalNoOfHrsWorked = totalNoOfHrsWorked + is_fullTimeWorkingHrs;
+				totalNoOfDaysWorked = totalNoOfDaysWorked + 1;
+				totalEmpWagePerDay = is_fullTimeWorkingHrs * empWagePerHr;
+				totalMontlyWageOfEmp = totalMontlyWageOfEmp + totalEmpWagePerDay;
+				break;
+			case 2:
+				totalNoOfHrsWorked = totalNoOfHrsWorked + is_fullTimeWorkingHrs;
+				totalNoOfDaysWorked = totalNoOfDaysWorked + 1;
+				totalEmpWagePerDay = is_partTimeWorkingHrs * empWagePerHr;
+				totalMontlyWageOfEmp = totalMontlyWageOfEmp + totalEmpWagePerDay;
+				break;
+			default:
+				totalNoOfDaysWorked = totalNoOfDaysWorked + 1;
 			}
 		}
 		System.out.println("total montly wage of an emp is : " + totalMontlyWageOfEmp);
